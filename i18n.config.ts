4676f0e7d02fcbc13377e2b3eb0d54b476430b06
asatter833 <asatter833@gmail.com>
2025-11-14 22:27:08 +0600

    added locale language list from the env

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,5 +1,5 @@
 const i18nConfig = {
-  locales: ["en", "bn", "pt-PT", "tr"],
+  locales: process.env.I18NEXUS_DEFAULT_LOCALE_LANGUAGE,
   defaultLocale: process.env.I18NEXUS_DEFAULT_LOCALE || "en",
 };
 
