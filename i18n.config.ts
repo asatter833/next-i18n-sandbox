0fdd9a1ef6b13bfc3c34964dfd2a91bb94b5376c
asatter833 <asatter833@gmail.com>
2025-11-14 22:24:25 +0600

    added default locale from the env file so that its easier to maintain while deploying in different country

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,6 +1,6 @@
 const i18nConfig = {
   locales: ["en", "bn", "pt-PT", "tr"],
-  defaultLocale: "en",
+  defaultLocale: process.env.I18NEXUS_DEFAULT_LOCALE || "en",
   // prefixDefault: true, // this will add the default local routing to the url eg: "/en/about" without it it would be "/about"
 };
 
