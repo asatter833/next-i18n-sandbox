0e962d171d789f4ecc9e121896a7a5cb25fdfc6b
asatter833 <asatter833@gmail.com>
2025-11-14 22:25:17 +0600

    removed  "prefixDefault" because its true by default

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,7 +1,6 @@
 const i18nConfig = {
   locales: ["en", "bn", "pt-PT", "tr"],
   defaultLocale: process.env.I18NEXUS_DEFAULT_LOCALE || "en",
-  // prefixDefault: true, // this will add the default local routing to the url eg: "/en/about" without it it would be "/about"
 };
 
 export default i18nConfig;
